2017a58797c803e6d85526af3a5872e1875c71de
strangem1n <strangem111n@gmail.com>
2026-04-28 06:28:58 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n = input().rstrip()
+if n == "SONGDO":
+    print("HIGHSCHOOL")
+elif n == "CODE":
+    print("MASTER")
+elif n == "2023":
+    print("0611")
+elif n == "ALGORITHM":
+    print("CONTEST")
