514d8df42bc787bb5c6817c3ab262f2dcaf20c43
strangem1n <strangem111n@gmail.com>
2026-04-28 06:16:40 +0900

    [Bronze III] Title: TGN, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    r, e, c = map(int, input().split())
+    if r < e-c:
+        print("advertise")
+    elif r == e-c:
+        print("does not matter")
+    else:
+        print("do not advertise")
