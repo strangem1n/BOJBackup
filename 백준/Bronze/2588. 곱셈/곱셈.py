481d6b06d37120dd4547ea8472a41ac200aedb5c
strangem1n <strangem111n@gmail.com>
2026-04-28 07:27:44 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = input()
+blist = []
+
+for i in b:
+    blist.append(i)
+one = a * int(blist[2])
+ten = a * int(blist[1])
+hun = a * int(blist[0])
+result = a * int(b)
+
+print(one)
+print(ten)
+print(hun)
+print(result)
