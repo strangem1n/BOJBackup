7e80f3cf6f5644373c497c5f6b09333bbbffee3b
strangem1n <strangem111n@gmail.com>
2026-04-28 06:30:56 +0900

    [Bronze IV] Title: 2023은 무엇이 특별할까?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if (n+1) % (n%100) == 0:
+        print('Good')
+    else:
+        print('Bye')
