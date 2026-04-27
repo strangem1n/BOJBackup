609718ae9ed9317dc57fb991f831e89dc8d97ed6
strangem1n <strangem111n@gmail.com>
2026-04-28 06:48:02 +0900

    [Gold IV] Title: 볼링장 아르바이트, Time: 156 ms, Memory: 143424 KB -BaekjoonHub

---
+++ 백준/Gold/27979. 볼링장 아르바이트/볼링장 아르바이트.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+balls = list(map(int, input().split()))
+sorted_balls = sorted(balls)
+
+idx = -1
+for i in range(n-1, -1, -1):
+    if balls[i] == sorted_balls[idx]:
+        n -= 1
+        idx -= 1
+
+print(n)
