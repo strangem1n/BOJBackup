cb9679e9cb0641e55d9af0c18f6e1c333de6d923
strangem1n <strangem111n@gmail.com>
2026-04-28 06:20:14 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,13 @@
+import sys
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    print("@@@@@"*n)
+for _ in range(n):
+    print("@"*n)
+for _ in range(n):
+    print("@@@@@"*n)
+for _ in range(n):
+    print("@"*n)
+for _ in range(n):
+    print("@@@@@"*n)
