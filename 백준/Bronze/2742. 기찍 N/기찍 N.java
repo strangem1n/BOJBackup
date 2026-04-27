34fda559c053fbe809a92d56cdcb709c636cb251
strangem1n <strangem111n@gmail.com>
2026-04-28 07:10:49 +0900

    [Bronze V] Title: 기찍 N, Time: 832 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int n=sc.nextInt(); n>0; n=n-1) {
+            System.out.println(n);
+        }
+    }
+}
